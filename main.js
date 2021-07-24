d64140e653d9555f0862dc5efd116ffa7c399de3
Jeevansh12 <76777313+Jeevansh12@users.noreply.github.com>
2021-07-24 15:54:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ function preload() {
 function setup() {
     canvas = createCanvas(300, 300);
     canvas.center();
-    video = creatCapture(Video);
-    video.size(300, 300);
+    video = createCapture(VIDEO);
+    video.size(300,300);
     video.hide();
     posenet = ml5.poseNet(video,modelLoaded);
-    poseNet.on('pose',gotPoses);
+    posenet.on('pose',gotPoses);
 }
 function gotPoses(results)
 {
@@ -23,7 +23,7 @@ function modelLoaded() {
     console.log('PoseNet is Initialized');
 }
 function draw() {
-    Image(video,0,0,300,300);
+    image(video,0,0,300,300);
 }
 function take_snapshot() {
     save('myFilterImage.png');
