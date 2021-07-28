078f880098ac5af759d7d75b2709a40898acd003
Jeevansh12 <76777313+Jeevansh12@users.noreply.github.com>
2021-07-28 16:47:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-function preload() {
+ noseX = 0;
+ noseY = 0;
+ function preload() {
+    mustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
 }
 
 function setup() {
     canvas = createCanvas(300, 300);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(300,300);
+    video.size(300, 300);
     video.hide();
     posenet = ml5.poseNet(video,modelLoaded);
     posenet.on('pose',gotPoses);
@@ -15,8 +18,11 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x = + results[0].pose.nose.x");
-        console.log("nose y = + results[0].pose.nose.y");
+        noseX = results[0].pose.nose.x-14;
+        noseY = results[0].pose.nose.y+5;
+        console.log("nose x = "+ noseX);
+        console.log("nose y = "+ noseY);
+    
     }
 }
 function modelLoaded() {
@@ -24,6 +30,10 @@ function modelLoaded() {
 }
 function draw() {
     image(video,0,0,300,300);
+    image(mustache, noseX,noseY,30,30);
+    fill(255,0,0);
+    stroke(255,0,0);
+    
 }
 function take_snapshot() {
     save('myFilterImage.png');
